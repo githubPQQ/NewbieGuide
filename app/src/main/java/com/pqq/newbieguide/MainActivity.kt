950fed75e001df705c5036f3462a1a5bc1e46e40
xiluo <xiluo@meili-inc.com>
2020-07-15 00:49:23 +0800

    dialog作为载体

--- app/src/main/java/com/pqq/newbieguide/MainActivity.kt
+++ app/src/main/java/com/pqq/newbieguide/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.pqq.newbieguide
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
+import com.pqq.lib.GuideDialog
 import com.pqq.lib.HellowData
 import com.pqq.lib.widget.GuideView
 
@@ -15,12 +18,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_new)
-        var guideView: GuideView = findViewById(R.id.guide_view)
         var tv: TextView = findViewById(R.id.tv)
+//        var guideView: GuideView = findViewById(R.id.guide_view)
 
         var hellowData: HellowData = HellowData()
         hellowData.hellowView = tv
+
+        var guideView: GuideView = GuideView(this)
         guideView.setHellowDatas(arrayOf(hellowData))
+
+        var tipView: View =
+            LayoutInflater.from(this).inflate(R.layout.layout_test_tip, null, false);
+
+        var builder: GuideDialog.Builder = GuideDialog.Builder()
+        builder.buildGuideView(guideView)
+            .buildOperateView(tipView)
+            .build()
+            .show(supportFragmentManager, "GuideDialog")
+
+
     }
 
 }
--- lib/src/main/java/com/pqq/lib/GuideDialog.java
+++ lib/src/main/java/com/pqq/lib/GuideDialog.java
@@ -1,10 +1,94 @@
 package com.pqq.lib;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
+
+import com.pqq.lib.widget.GuideView;
 
 public class GuideDialog extends DialogFragment {
 
+    /**
+     * 镂空图层
+     */
+    private GuideView guideView;
 
+    /**
+     * 操作图层
+     */
+    private View operateView;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL,R.style.Dialog_FullScreen);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (view instanceof ViewGroup) {
+
+            if (guideView != null) {
+                ((ViewGroup) view).addView(guideView);
+            }
+
+            if (guideView != null) {
+                ((ViewGroup) view).addView(operateView);
+            }
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_guide_layout, container, false);
+        return view;
+    }
+
+    public void setGuideView(GuideView guideView) {
+        this.guideView = guideView;
+    }
+
+    public void setOperateView(View operateView) {
+        this.operateView = operateView;
+    }
+
+    public static class Builder {
+        /**
+         * 镂空图层
+         */
+        private GuideView guideView;
+
+        /**
+         * 操作图层
+         */
+        private View operateView;
+
+        public Builder buildGuideView(GuideView guideView) {
+            this.guideView = guideView;
+            return this;
+        }
+
+        public Builder buildOperateView(View operateView) {
+            this.operateView = operateView;
+            return this;
+        }
+
+        public GuideDialog build() {
+            GuideDialog guideDialog = new GuideDialog();
+            guideDialog.setGuideView(guideView);
+            guideDialog.setOperateView(operateView);
+            return guideDialog;
+        }
+
+    }
 
 }
