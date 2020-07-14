5945662c2d04038c09717ebaa95723c87f8e20e8
xiluo <xiluo@meili-inc.com>
2020-07-15 00:02:15 +0800

    测试

--- app/src/main/java/com/pqq/newbieguide/MainActivity.kt
+++ app/src/main/java/com/pqq/newbieguide/MainActivity.kt
@@ -6,13 +6,21 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import com.pqq.lib.HellowData
+import com.pqq.lib.widget.GuideView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_new)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        var guideView: GuideView = findViewById(R.id.guide_view)
+        var tv: TextView = findViewById(R.id.tv)
+
+        var hellowData: HellowData = HellowData()
+        hellowData.hellowView = tv
+        guideView.setHellowDatas(arrayOf(hellowData))
     }
 
 }
---
+++ lib/src/main/java/com/pqq/lib/GuideDialog.java
@@ -0,0 +1,17 @@
+package com.pqq.lib;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class GuideDialog extends DialogFragment {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
